29a1bef53c0efa1f2b115f0a745e268190a87e29
Viktoria <doduchenko29@gmail.com>
2021-06-21 18:42:33 +0300

    add

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 export const printing = () => {
   console.log(1);
   setTimeout(function() {
-    console.log(2)
+    console.log(5)
   }, 1000);
-  console.log(3);
+  console.log(2);
   setTimeout(function() {
     console.log(4)
   }, 0);
-  console.log(5)
+  console.log(3)
 }
 printing()
